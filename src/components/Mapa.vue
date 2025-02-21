4b5da39df0adc0c3f4103d90186c410d007eefb9
João Palma <palmajoaodiogo@gmail.com>
2025-02-21 14:21:55 +0000

    Fix marker

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { ref, onMounted, watch } from "vue";
 import L from "leaflet";
+import markerIcon from '/img/marker-icon.png';
+import markerIcon2x from '/img/marker-icon-2x.png';
+import markerShadow from '/img/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 const props = defineProps({
   idEvento: { type: Number, required: true } // O ID do evento a ser mostrado no mapa
